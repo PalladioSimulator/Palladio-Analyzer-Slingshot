ca00b3246b06bf58231e18485b78c044f3102ede
Sarah Sophie Stieß <sarah.stiess@iste.uni-stuttgart.de>
2025-05-27 07:41:44 +0200

    remove duplicate id.

--- bundles/org.palladiosimulator.analyzer.slingshot.common/src/org/palladiosimulator/analyzer/slingshot/common/events/AbstractEntityChangedEvent.java
+++ bundles/org.palladiosimulator.analyzer.slingshot.common/src/org/palladiosimulator/analyzer/slingshot/common/events/AbstractEntityChangedEvent.java
@@ -5,17 +5,10 @@ import java.util.Objects;
 public abstract class AbstractEntityChangedEvent<T> extends AbstractSimulationEvent {
 
 	private final T entity;
-	private final String id;
-	
+
 	public AbstractEntityChangedEvent(final T entity, final double delay) {
-		super("", delay);
+		super(delay);
 		this.entity = Objects.requireNonNull(entity);
-		this.id = String.format("%s-%X", getClass().getSimpleName(), entity.hashCode());
-	}
-	
-	@Override
-	public String getId() {
-		return id;
 	}
 	
 	public T getEntity() {
